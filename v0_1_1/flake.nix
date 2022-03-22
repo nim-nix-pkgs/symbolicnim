e3579c8e9b1a33a5fdabc76a8e9c85a9f8417d1f
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:16 -0300

    chore: re-index ../nimpkgs/s/symbolicnim

--- v0_1_1/flake.nix
+++ v0_1_1/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''A symbolic library written purely in Nim with the ability to compile expressions into efficient functions.'';
+  description = ''A symbolic algebra library written in Nim'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-symbolicnim-v0_1_1.flake = false;
   inputs.src-symbolicnim-v0_1_1.owner = "HugoGranstrom";
-  inputs.src-symbolicnim-v0_1_1.ref   = "refs/tags/v0.1.1";
+  inputs.src-symbolicnim-v0_1_1.ref   = "v0_1_1";
   inputs.src-symbolicnim-v0_1_1.repo  = "symbolicnim";
   inputs.src-symbolicnim-v0_1_1.type  = "github";
   
--- v0_2_0/flake.nix
+++ v0_2_0/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''A symbolic library written purely in Nim with the ability to compile expressions into efficient functions.'';
+  description = ''A symbolic algebra library written in Nim'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-symbolicnim-v0_2_0.flake = false;
   inputs.src-symbolicnim-v0_2_0.owner = "HugoGranstrom";
-  inputs.src-symbolicnim-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-symbolicnim-v0_2_0.ref   = "v0_2_0";
   inputs.src-symbolicnim-v0_2_0.repo  = "symbolicnim";
   inputs.src-symbolicnim-v0_2_0.type  = "github";
   
